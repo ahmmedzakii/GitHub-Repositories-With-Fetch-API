eaf06f03e45ad29b2907ed99d6f293cdf304f7dd
Ahmed Zaki <ahmedzakizakijr@gmail.com>
2025-09-15 04:58:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// Start Ge Main Variabels
+let input = document.querySelector("input");
+let button = document.querySelector(".get-button");
+let mainShowData = document.querySelector(".show-data");
+
+button.onclick = function () {
+  getRepos();
+};
+
+function getRepos() {
+  if (input.value === "") {
+    console.log("Please Enter Avalid User name");
+  } else {
+    fetch(`https://api.github.com/users/${input.value}/repos`)
+      .then((rebos) => rebos.json())
+
+      .then((repostries) => {
+        console.log(repostries);
+        repostries.forEach((repo) => {
+          //creat Elements
+          let div = document.createElement("div");
+          let pName = document.createElement("p");
+          pName.className = "name";
+          pName.appendChild(document.createTextNode(`Name: ${repo["name"]}`));
+
+          let pid = document.createElement("p");
+          pid.className = "id";
+          pid.appendChild(document.createTextNode(`Id ${repo["id"]}`));
+
+          let anqour = document.createElement("a");
+          anqour.className = "classFora";
+          anqour.appendChild(document.createTextNode(`Link`));
+          anqour.href = `https://github.com/${input.value}/${repo["name"]}`;
+          anqour.setAttribute('target' , '_blank');
+
+          let pDate = document.createElement("p");
+          pDate.className = "date";
+          pDate.appendChild(
+            document.createTextNode(`Publish ${repo["pushed_at"].slice(0, 10)}`)
+          );
+
+          // Append Part
+          div.appendChild(pName);
+          div.appendChild(pid);
+          div.appendChild(anqour);
+          div.appendChild(pDate);
+
+          mainShowData.appendChild(div);
+        });
+      });
+  }
+}
+
+
+let footer = document.querySelector(".year");
+let yeraDate = new Date();
+footer.innerHTML=yeraDate.getFullYear()
+
